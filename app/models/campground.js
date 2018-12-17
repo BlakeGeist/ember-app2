1af324ab996147878b8b86c5b89d580eb7940b94
BlakeGeist <blakegeist@gmail.com>
2018-12-17 13:37:42 -0800

    updates for default image data
    updates for default image data

--- app/models/campground.js
+++ app/models/campground.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  image: DS.attr()
+  image: DS.attr({
+    defaultValue(){
+      return 'https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest/scale-to-width-down/600?cb=20170330235552'
+    }
+  })
 });
